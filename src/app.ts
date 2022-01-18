62bd1c1c8f58acae3f52dc3389d15ca2bf278b88
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-18 16:01:08 +0300

    add OOP rendering of html form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-// Code goes here!
+/*
+Display the form when the class is instantiated
+*/
+class TaskInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    docElement: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
+        this.hostElement = <HTMLDivElement>document.getElementById('app')!;
+
+        const docFragment = document.importNode(this.templateElement.content, true);
+        this.docElement = <HTMLFormElement>docFragment.firstElementChild;
+        this.attachElement();
+
+    }
+
+    private attachElement() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.docElement);
+    }
+}
+
+new TaskInput();
