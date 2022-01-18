a04a06edb87f80182a1a41849c2beaba4783da40
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-18 16:26:01 +0300

    interacting with dom elements

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ class TaskInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     docElement: HTMLFormElement;
+    titleInput: HTMLInputElement;
+    descriptionInput: HTMLInputElement;
+    peopleInput: HTMLInputElement;
 
     constructor() {
         this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
@@ -12,9 +15,28 @@ class TaskInput {
 
         const docFragment = document.importNode(this.templateElement.content, true);
         this.docElement = <HTMLFormElement>docFragment.firstElementChild;
+        this.docElement.id = 'user-input';
+        this.titleInput = <HTMLInputElement>this.docElement.querySelector('#title')!;
+        this.descriptionInput = <HTMLInputElement>this.docElement.querySelector('#description')!;
+        this.peopleInput = <HTMLInputElement>this.docElement.querySelector('#people')!;
+
+        this.config();
         this.attachElement();
 
     }
+
+    private submitHandler (e: Event) {
+        e.preventDefault();
+        console.log({
+            title: this.titleInput.value,
+            description: this.descriptionInput.value,
+            people: this.peopleInput.value
+        })
+    }
+    private config() {
+        this.docElement.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
 
     private attachElement() {
         this.hostElement.insertAdjacentElement('afterbegin', this.docElement);
